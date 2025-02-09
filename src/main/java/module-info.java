1fc109159ef2bd73cbfe8935e2b781fa27c382e7
torakiki <andrea.vacondio@gmail.com>
2025-02-09 12:47:55 +0100

    Added module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,19 @@
+/*
+ * This file is part of the Autopo project
+ * Created 05/02/25
+ * Copyright 2025 by Sober Lemur S.r.l. (info@soberlemur.com).
+ *
+ * You are not permitted to distribute it in any form unless explicit
+ * consent is given by Sober Lemur S.r.l..
+ * You are not permitted to modify it.
+ *
+ * Autopo is distributed WITHOUT ANY WARRANTY;
+ * without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+module com.soberlemur.potentilla {
+    exports com.soberlemur.potentilla;
+    exports com.soberlemur.potentilla.catalog.parse;
+    requires antlr;
+    requires org.slf4j;
+}
